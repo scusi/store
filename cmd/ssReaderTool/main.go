c7adb5f506cd6bb72ad817dfabe9b4b6f9a90e23
Florian Walther <florian.walther@certainity.com>
2024-01-09 09:24:22 +0100

    adding Stream tool

---
+++ cmd/ssReaderTool/main.go
@@ -0,0 +1,48 @@
+// tool to use the store module
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/scusi/scusiStore"
+	"os"
+)
+
+func main() {
+	// create a new scusiStore under the given path
+	ns := store.New("/home/flow/.scusiStore")
+	fmt.Printf("%#v\n", ns)
+
+	// if a filename argument was given add that file to scusiStore
+	//
+	fileName := os.Args[1]
+	if fileName != "" {
+		// first open a filehandle
+		file, err := os.Open(fileName)
+		if err != nil {
+			panic(err)
+		}
+		// turn filehandle into an io.Reader for that file
+		fileReader := bufio.NewReader(file)
+		// call WriteStream to add the file to scusiStore
+		id, err := ns.WriteStream(fileName, fileReader, true)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("stored under fileID: %s\n", id)
+	} else {
+		fmt.Println("No filename given")
+		os.Exit(1)
+	}
+
+	// get list of all files in scusiStore
+	l, err := ns.List()
+	if err != nil {
+		panic(err)
+	}
+	// print the list of files
+	fmt.Printf("Filelist:\n%+v\n", l)
+	for k, v := range l {
+		fmt.Printf("%d\t%+v\n", k, v)
+	}
+}
